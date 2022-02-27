98a2259aaf9f52a628929b2fd4278ce92a0c3fd1
Laurence Howard <laurence.william.howard@gmail.com>
2022-02-27 17:43:21 +0000

    Clean up unnessecary files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
-import "../styles/globals.css";
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
 import { AppProps } from "next/app";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <div
+      css={css`
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      `}
+    >
       <Head>
         <link rel="shortcut icon" href="/bracket-favicon.png" />
       </Head>
