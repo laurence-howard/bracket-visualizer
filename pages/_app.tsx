6f54c1d241af18799ccc8c17912280d6638ddb23
Laurence Howard <laurence.william.howard@gmail.com>
2022-02-27 17:38:30 +0000

    Update readme and update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <link rel="shortcut icon" href="/bracket-favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
